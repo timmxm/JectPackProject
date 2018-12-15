f73ff86869ce7f9d50d7c8372233ee256bd9f958
Thanongsak <thanongsak_n@cmu.ac.th>
2018-12-15 20:01:26 +0700

    finished all XML

---
+++ app/src/main/java/com/example/mahti/jetpackproject/EditContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.mahti.jetpackproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact);
+    }
+}
