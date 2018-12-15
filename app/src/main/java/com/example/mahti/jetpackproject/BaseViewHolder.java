77bba5edcff362bca6c53fe061767a3650905679
Alicis <natthapong_chan@cmu.ac.th>
2018-12-16 00:22:32 +0700

    noob

---
+++ app/src/main/java/com/example/mahti/jetpackproject/BaseViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.mahti.jetpackproject;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+// Base class for view holder
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+
+}
