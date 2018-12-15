c45bf6d61385e6d17e9d5573ad3852fdadbf0cb6
Ronnakit Boonpuncher <feadtodark@gmail.com>
2018-12-16 04:57:28 +0700

    add intent for userActivity

--- app/src/main/java/com/example/mahti/jetpackproject/UserActivity.java
+++ app/src/main/java/com/example/mahti/jetpackproject/UserActivity.java
@@ -1,14 +1,50 @@
 package com.example.mahti.jetpackproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class UserActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Button btn_logout;
+    private Button btn_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        this.btn_logout = (Button) findViewById(R.id.btn_logout);
+        this.btn_edit = (Button) findViewById(R.id.btn_edit);
+
+        this.btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+
+                Intent i = new Intent(UserActivity.this, LoginActivity.class);
+                UserActivity.this.startActivity(i);
+            }
+        });
+
+        this.btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserActivity.this, RegisterActivity.class);
+                UserActivity.this.startActivity(i);
+            }
+        });
+
     }
 }
