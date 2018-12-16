1550d958c466aa8e6efe963d272191eaeb312a97
Natthapol <chameloengat@gmail.com>
2018-12-16 23:51:33 +0700

    Changes: UserActivity is done Change all placeholder fields to "Loading..."

--- app/src/main/java/com/example/mahti/jetpackproject/UserActivity.java
+++ app/src/main/java/com/example/mahti/jetpackproject/UserActivity.java
@@ -1,5 +1,6 @@
 package com.example.mahti.jetpackproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,10 +12,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class UserActivity extends AppCompatActivity {
 
+    private FirebaseUser user;
     private FirebaseAuth mAuth;
 
     private Button btn_logout;
@@ -24,7 +32,6 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-
 
         // back
         ImageView back_icon = findViewById(R.id.user_back_icon);
@@ -45,6 +52,34 @@ public class UserActivity extends AppCompatActivity {
         });
 
         mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null)
+        {
+
+            FirebaseUser user = mAuth.getCurrentUser();
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("cards/" + user.getUid());
+
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    CardDetail card = dataSnapshot.getValue(CardDetail.class);
+                    TextView username = findViewById(R.id.userPrompt_name);
+                    username.setText(card.getName());
+                    TextView bloodType = findViewById(R.id.user_blood);
+                    bloodType.setText(card.getBloodType());
+                    TextView allergy = findViewById(R.id.user_allergy);
+                    allergy.setText(card.getAllergies());
+                    TextView disease = findViewById(R.id.user_disease);
+                    disease.setText(card.getUnderlyingDisease());
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //An error, duh!
+                }
+
+            });
+        }
 
         this.btn_logout = (Button) findViewById(R.id.btn_logout);
         this.btn_edit = (Button) findViewById(R.id.btn_edit);
