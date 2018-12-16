d3f2e7fbdf79e7358650eaea0154b52669aa1fd5
Natthapol <chameloengat@gmail.com>
2018-12-16 23:10:08 +0700

    The top-right corner button is working again.

--- app/src/main/java/com/example/mahti/jetpackproject/MainActivity.java
+++ app/src/main/java/com/example/mahti/jetpackproject/MainActivity.java
@@ -7,7 +7,12 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -15,13 +20,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView icon_user;
     List<CardDetail> Card1st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 //Error, duh
+            }
+        });
+
+        this.icon_user = (ImageView) findViewById(R.id.icon_user);
+        this.icon_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this, UserActivity.class);
+                MainActivity.this.startActivity(i);
             }
         });
 
